6b2273d4c90513d930dff925386feb8950a12153
Bluey1562 <rizalakmal@idn.sch.id>
2026-01-15 14:50:46 +0700

    Update main.ts

--- main.ts
+++ main.ts
@@ -1,5 +1,22 @@
+/**
+ * ESP8266 WiFi Extension
+ */
+//% color=#0fbcf9 icon="\uf1eb" block="ESP8266 WiFi"
 namespace ESP8266 {
-    export function init(tx: SerialPin, rx: SerialPin, baud: number = 115200) {
+
+    /**
+     * Inisialisasi ESP8266
+     * @param tx pin TX
+     * @param rx pin RX
+     * @param baud baud rate
+     */
+    //% block="ESP8266 init TX %tx RX %rx baud %baud"
+    //% baud.defl=115200
+    export function init(
+        tx: SerialPin,
+        rx: SerialPin,
+        baud: number = 115200
+    ) {
         serial.redirect(tx, rx, baud)
         basic.pause(1000)
         send("AT")
@@ -8,6 +25,12 @@ namespace ESP8266 {
         basic.pause(1000)
     }
 
+    /**
+     * Connect ke WiFi
+     * @param ssid nama wifi
+     * @param password password wifi
+     */
+    //% block="ESP8266 connect WiFi SSID %ssid PASSWORD %password"
     export function connectWiFi(ssid: string, password: string) {
         send(`AT+CWJAP="${ssid}","${password}"`)
         basic.pause(5000)
@@ -17,3 +40,4 @@ namespace ESP8266 {
         serial.writeString(cmd + "\r\n")
     }
 }
+
